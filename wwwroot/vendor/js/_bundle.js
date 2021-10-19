5cf17e39feab00b0ba5d2d86bfe9fd6645e9942e
Nikolay Gruychev <ngruychev@gmail.com>
2021-10-20 02:09:09 +0300

    feat: add 3rd party js and css libs

---
+++ wwwroot/vendor/js/_bundle.js
@@ -0,0 +1,12 @@
+// big fat bundle. bundle with `deno bundle _bundle.js > bundle.js`, minify with `esbuild --minify bundle.js | sponge bundle.js`
+export * from "https://cdn.skypack.dev/preact";
+export * from "https://cdn.skypack.dev/preact/hooks";
+export * from "https://cdn.skypack.dev/nanostores";
+export * from "https://cdn.skypack.dev/nanostores/preact";
+export * from "https://cdn.skypack.dev/nanostores/map-template";
+export * from "https://cdn.skypack.dev/nanostores/atom";
+export * from "https://cdn.skypack.dev/nanostores/lifecycle";
+export * from "https://cdn.skypack.dev/nanostores/task";
+export * from "https://cdn.skypack.dev/@nanostores/router";
+export { html } from "https://cdn.skypack.dev/htm/preact";
+export { css } from "https://gistcdn.githack.com/zserge/b8d4fdfb544617ddf4df74c092063501/raw/2ae1fb823efb62cb26f15763b1130ab395aa8206/zercss.js";
